dbf793144efb0c111b72590e0ab615ed994d9ec7
aatitkarki <aatitkarki123@gmail.com>
2023-03-16 23:36:00 +0545

    added reponsive About section

---
+++ src/Main/About/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./About.css";
+import { about } from "../../assets";
+
+function About() {
+  return (
+    <section className='section' id='about'>
+      <div className='section-title'>
+        <h2>
+          About <span>Us</span>
+        </h2>
+      </div>
+
+      <article className='section-center about-center'>
+        <div className='about-image'>
+          <img src={about} alt='Tour 1' className='about-photo' />
+        </div>
+
+        <div className='about-info'>
+          <h3>Explore The Difference</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur quisquam harum nam
+            cumque temporibus explicabo dolorum sapiente odio unde dolor?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur quisquam harum nam
+            cumque temporibus explicabo dolorum sapiente odio unde dolor?
+          </p>
+          <a href='#12' className='btn'>
+            Read More
+          </a>
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default About;
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import About from "./About/About";
 import Hero from "./Hero/Hero";
 
 function Main() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
